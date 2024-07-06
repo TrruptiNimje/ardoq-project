5a4a28cab3db621039879a1d22a33e5284fd803e
trruptinimje <nimje.trupti5@gmail.com>
2024-07-06 16:26:55 +0200

    Added detailed test report

--- conftest.py
+++ conftest.py
@@ -2,13 +2,15 @@ import pytest
 from selenium import webdriver
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def driver(request):
+    # Setup: Initialize the WebDriver instance
     print("Creating Chrome driver")
     my_driver = webdriver.Chrome()
     my_driver.maximize_window()
     my_driver.implicitly_wait(20)
     yield my_driver
+    # Teardown: Close the WebDriver instance
     print("Closing Chrome driver")
     my_driver.quit()
 
@@ -22,8 +24,8 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="session")
 def user_credentials():
     return {
-        "first_name": "Ola",
-        "last_name": "Richi",
-        "email": "olarichi11@mail.com",
+        "first_name": "Tom",
+        "last_name": "Cruise",
+        "email": "tomcruise72@mail.com",
         "password": "Qwer@1234"
     }
